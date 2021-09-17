c03fab79dcb9e673cb3b3c8f8879c6910ec12260
AlexisFarfan <jumpytunechi@gmail.com>
2021-09-17 17:17:33 +0200

    Ejercicio1.a
    Variables con ámbito privado.

---
+++ PR01-DEIM/Assets/Scrpts/Ejercicio1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour
+{
+    private int num1 = 1;
+    private float num2 = 3.5f;
+    private bool alive = true;
+    private string saludo = "hola";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(num1);
+        print(num2);
+        print(alive);
+        print(saludo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
