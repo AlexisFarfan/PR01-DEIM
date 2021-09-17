efc31a4359fba129d02252eade3f7e201b4d80c5
AlexisFarfan <jumpytunechi@gmail.com>
2021-09-17 19:20:30 +0200

    Ejercicio2.b
    Bucle con while y for.

--- PR01-DEIM/Assets/Scrpts/Ejercicio2.cs
+++ PR01-DEIM/Assets/Scrpts/Ejercicio2.cs
@@ -4,30 +4,29 @@ using UnityEngine;
 
 public class Ejercicio2 : MonoBehaviour
 {
-    private int num1= 1;
+   // private int num1= 1;
 
     // Start is called before the first frame update
     void Start()
-    {
-       
+    {/*
+       while(num1<=10)
+        {
+            print(num1);
+            num1++;
+        }
+        */
+        for (int num2 = 1;num2<=10;num2++)
+        {
 
-
+            print(num2);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        //es mas util el + que el ++ 
-
-        if (num1 <= 100)
-        {
-            
-           print(num1);
-        }
-        //el ++ resumea la operacion
-        num1++;
-
+       
 
     }
 }
