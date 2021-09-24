e95a1502a3bc0d959c9496a63fc12ad881a55061
AlexisFarfan <jumpytunechi@gmail.com>
2021-09-24 20:27:30 +0200

    Ejercicio4.b
    Gameobject que se mueve de forma vertical y horizontal, controlántolo con el gamepad o teclado.

---
+++ PR01-DEIM/Assets/Scrpts/Ejercicio4.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio4 : MonoBehaviour
+{
+    [SerializeField] float speed = 15f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moverY = Input.GetAxis("Vertical") * speed;
+        transform.Translate(Vector3.up * moverY * Time.deltaTime);
+
+
+        float moverX = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(Vector3.left * moverX * Time.deltaTime);
+
+        
+    }
+}
