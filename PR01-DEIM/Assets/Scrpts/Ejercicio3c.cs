d27e835a6d152ad4bb1342009c87db149369166c
AlexisFarfan <jumpytunechi@gmail.com>
2021-09-24 13:47:39 +0200

    Ejercicio3.c.a
    Creación de un reloj con segundos, minutos y hora.

--- PR01-DEIM/Assets/Scrpts/Ejercicio3c.cs
+++ PR01-DEIM/Assets/Scrpts/Ejercicio3c.cs
@@ -6,23 +6,34 @@ using UnityEngine.UI;
 public class Ejercicio3c : MonoBehaviour
 {
     [SerializeField] Text contador;
+  
 
     // Start is called before the first frame update
     void Start()
     {
 
-        StartCoroutine("Cronometro");
+        StartCoroutine("Contador");
        
     }
 
     // Update is called once per frame
    
-    IEnumerator Cronometro()
+    IEnumerator Contador()
     {
-        for (int n = 0; n <= 20; n++)
+        
+        
+        for (int h = 0; h<=60 ; h++)
         {
-            contador.text = " " + n;
-            yield return new WaitForSeconds(1f);
+        
+            for (int m =0; m<=60; m++)
+            {
+                for(int s=0; s<=60; s++)
+                {
+                    contador.text = h + ":" + m + ":" + s;
+                    yield return new WaitForSeconds(1f);
+                }
+            }
         }
+
     }
 }
