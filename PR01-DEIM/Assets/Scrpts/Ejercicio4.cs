e37d4af3fb5d08be19255425e51937ccb734009e
AlexisFarfan <jumpytunechi@gmail.com>
2021-09-24 23:25:37 +0200

    Ejercicio4.b.b
    Añadido tercer control para que se mueva hacia adelante presionando "i" ,o, hacia atrás presionando "k".

--- PR01-DEIM/Assets/Scrpts/Ejercicio4.cs
+++ PR01-DEIM/Assets/Scrpts/Ejercicio4.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class Ejercicio4 : MonoBehaviour
 {
     [SerializeField] float speed = 15f;
-
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //transform.position = InitPos;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float moverY = Input.GetAxis("Vertical") * speed;
         transform.Translate(Vector3.up * moverY * Time.deltaTime);
 
@@ -23,6 +24,11 @@ public class Ejercicio4 : MonoBehaviour
         float moverX = Input.GetAxis("Horizontal") * speed;
         transform.Translate(Vector3.left * moverX * Time.deltaTime);
 
-        
+        float moverZ = Input.GetAxis("Profundidad") * speed;
+        transform.Translate(Vector3.back * moverZ * Time.deltaTime);
+
+      
+
+
     }
 }
